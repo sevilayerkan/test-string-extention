3a7edf9439a78533b6839a3ab2ccc86874b6e644
Sevilay Erkan <22706692+sevilayerkan@users.noreply.github.com>
2024-05-07 21:55:18 +0000

    chore: refactor js files

--- popup.js
+++ popup.js
@@ -6,36 +6,3 @@ document.addEventListener("DOMContentLoaded", function () {
     copyButton.addEventListener("click", copyToClipboard);
 });
 
-function generateString() {
-    const lengthInput = document.getElementById("lengthInput").value;
-    const removePunctInput = document.getElementById("removePunct").checked;
-    const removeSpaceInput = document.getElementById("removeSpace").checked;
-    const resultText = document.getElementById("resultText");
-
-    const loremIpsumWordsInput = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisl eget ultricies ultricies, nunc nisl aliquam nunc, vitae aliquam";
-
-    const length = parseInt(lengthInput);
-    const useSpace = removeSpaceInput;
-    const usePunct = removePunctInput;
-
-    const generatedString = generateText(loremIpsumWordsInput, length, useSpace, usePunct);
-    resultText.value = generatedString;
-}
-
-function generateText(text, length, useSpace, usePunct) {
-    let resultText = text.repeat(Math.ceil(length / text.length)).substring(0, length);
-
-    if (usePunct) {
-        resultText = resultText.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
-    }
-    if (useSpace) {
-        resultText = resultText.replace(/\s+/g, '');
-    }
-
-    return resultText;
-}
-
-function copyToClipboard() {
-    const resultText = document.getElementById("resultText");
-    navigator.clipboard.writeText(resultText.value);
-}
