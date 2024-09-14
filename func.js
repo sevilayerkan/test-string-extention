12c588907d3bbdbbb7cdfa34dd9170ee83cd8a1e
sevilayerkan <sevilayerkan2@gmail.com>
2024-09-15 01:51:11 +0300

    chore: refactor whole project

--- func.js
+++ func.js
@@ -1,35 +1,24 @@
-function generateText(text, length, useSpace, usePunct) {
-    let resultText = text.repeat(Math.ceil(length / text.length)).substring(0, length);
+// Constants
+const LOREM_IPSUM_TEXT =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisl eget ultricies ultricies, nunc nisl aliquam nunc, vitae aliquam";
+const PUNCTUATION_REGEX = /[.,\/#!$%\^&\*;:{}=\-_`~()]/g;
+const WHITESPACE_REGEX = /\s+/g;
 
-    if (usePunct) {
-        resultText = resultText.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
-    }
-    if (useSpace) {
-        resultText = resultText.replace(/\s+/g, '');
-    }
+// Core functionality
+function generateLoremIpsum(length, removeSpace, removePunct) {
+  let result = LOREM_IPSUM_TEXT.repeat(
+    Math.ceil(length / LOREM_IPSUM_TEXT.length)
+  ).substring(0, length);
 
-    return resultText;
+  if (removePunct) {
+    result = result.replace(PUNCTUATION_REGEX, "");
+  }
+  if (removeSpace) {
+    result = result.replace(WHITESPACE_REGEX, "");
+  }
+
+  return result;
 }
 
-function generateString() {
-    const lengthInput = document.getElementById("lengthInput").value;
-    const removePunctInput = document.getElementById("removePunct").checked;
-    const removeSpaceInput = document.getElementById("removeSpace").checked;
-    const resultText = document.getElementById("resultText");
-
-    const loremIpsumWordsInput = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisl eget ultricies ultricies, nunc nisl aliquam nunc, vitae aliquam";
-
-    const length = parseInt(lengthInput);
-    const useSpace = removeSpaceInput;
-    const usePunct = removePunctInput;
-
-    const generatedString = generateText(loremIpsumWordsInput, length, useSpace, usePunct);
-    resultText.value = generatedString;
-}
-
-
-
-function copyToClipboard() {
-    const resultText = document.getElementById("resultText");
-    navigator.clipboard.writeText(resultText.value);
-}
+// Expose function to be used in other files
+window.generateLoremIpsum = generateLoremIpsum;
